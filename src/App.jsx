19afd5f49a94dd997b43383a7da8c32446ad6df1
codingbutterflyyy <camilla.butterflydevs@gmail.com>
2025-04-02 13:27:19 +0200

    Display base delle carte per i film

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
+import {useState} from "react";
+
+const Card = ({title}) => {
+    const [hasLiked, setLiked] = useState(false);
+
+    return (
+        <div className={`card ${hasLiked ? 'card-liked' : 'card-disliked'}`}>
+            <h2>{title}</h2>
+            <button onClick={() => {setLiked(!hasLiked)}}>
+                {hasLiked ? "Liked" : "Like"}
+            </button>
+        </div>
+    )
+}
 
 function App() {
+
     return (
-        <div className="App">
-            <p>Ciao, mondo!</p>
+        <div className="card-container">
+            <Card title={"Star Wars"}></Card>
+            <Card title={"Avatar"}></Card>
+            <Card title={"The Lion King"}></Card>
         </div>
     )
 }
